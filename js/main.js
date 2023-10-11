79a5a1997a274df0213ce1ac8d640dab7ce123af
jonibek0979 <omadbekxoshimov20060715@gmail.com>
2023-10-11 11:24:51 +0500

    inital commit

---
+++ js/main.js
@@ -0,0 +1,130 @@
+var elB = document.querySelector('.b')
+var elB__1 = document.querySelector('.b__1')
+var elB__2 = document.querySelector('.b__2')
+var elB__3 = document.querySelector('.b__3')
+var elInp = document.querySelector('.inp')
+
+
+var piyoda = 4
+var valosiped = 20
+var mashina = 100
+var samalyot = 800
+
+
+function vaqt() {
+
+elB.textContent = elInp.value / piyoda
+elB__1.textContent = elInp.value / valosiped
+elB__2.textContent = elInp.value /  mashina
+elB__3.textContent = elInp.value /  samalyot
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sayohat__vaqti() {
+//     var distance = parseFloat(document.getElementById("distanceInput").value);
+//     var elText__1 = document.querySelector('.text__1');
+//     var elText__2 = document.querySelector('.text__2');
+//     var elText__3 = document.querySelector('.text__3');
+//     var elText__4 = document.querySelector('.text__4');
+//     var v_piyoda = 4;    // km/soat
+//     var v_velesaped = 20;   // km/soat
+//     var v_mashina = 100;   // km/soat
+//     var v_samolyot = 800;   // km/soat
+  
+//     var vaqt_piyoda = distance / v_piyoda;
+//     var vaqt_velesaped = distance / v_velesaped;
+//     var vaqt_mashina = distance / v_mashina;
+//     var vaqt_samolyot = distance / v_samolyot;
+  
+//     elText__1.textContent = vaqt_piyoda.toFixed(2) + " soat";
+//     elText__2.textContent = vaqt_velesaped.toFixed(2) + " soat";
+//     elText__3.textContent = vaqt_mashina.toFixed(2) + " soat";
+//     elText__4.textContent = vaqt_samolyot.toFixed(2) + " soat";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
